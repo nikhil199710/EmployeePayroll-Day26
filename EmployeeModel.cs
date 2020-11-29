0f73b2f871622de6e15041d6aa13ce7984f41c6c
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-29 20:36:57 +0530

    Update EmployeeModel.cs

--- EmployeeModel.cs
+++ EmployeeModel.cs
@@ -1,24 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ADO.NetDemo
+namespace ADO.net
 {
     public class EmployeeModel
     {
         public int EmpID { get; set; }
-        public string EmployeeName { get; set; }     
+        public string EmpName { get; set; }
         public double BasicPay { get; set; }
         public DateTime StartDate { get; set; }
         public string Gender { get; set; }
-        public long PhoneNo { get; set; }
+        public string PhnNo { get; set; }
         public string Department { get; set; }
         public string Address { get; set; }
-
-        public double Deductions { get; set; }
-        public double TaxablePay { get; set; }
-        public double Tax { get; set; }
-        public double NetPay { get; set; }
-        
+        public decimal Deductions { get; set; }
+        public decimal TaxablePay { get; set; }
+        public decimal IncomeTax { get; set; }
+        public decimal NetPay { get; set; }
     }
 }
