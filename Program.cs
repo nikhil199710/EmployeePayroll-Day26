91c292020ed2f6ebded8637b3d0f88fa9ddd807d
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-29 19:08:59 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,37 @@
-﻿using System;
+using System;
 
-namespace ADO.NetDemo
+namespace ADO.net
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            EmployeeRepository repository = new EmployeeRepository();
-            //repository.EnsureDataBaseConnection();
-            repository.GetAllEmployees();
+            Console.WriteLine("Hello World!");
+            EmployeeRepository employeeRepository = new EmployeeRepository();
 
+            ///Instance of the GetAllEmployees method
+            employeeRepository.GetAllEmployees();
+            EmployeeModel model = new EmployeeModel();
 
+            ///Adding employee deatils into the database
+            model.EmpName = "Riya";
+            model.BasicPay = 75000;
+            model.StartDate = DateTime.Now;
+            model.Gender = "F";
+            model.PhnNo = "7852149630";
+            model.Department = "IT";
+            model.Address = "Lucknow";
+            model.Deductions = 4540;
+            model.TaxablePay = 3204;
+            model.IncomeTax = 4500;
+            model.NetPay = 52000;
+            Console.WriteLine(employeeRepository.AddEmployee(model) ? "Record inserted successfully " : "Failed");
 
-            repository.UpdateSalary("Terisa");
-            //repository.GetAllEmployees();
-            //repository.UpdateEmployeeUsingStoredProcedure("Bill", 3000);
-            //repository.GetAllEmployeesBeforeToday(Convert.ToDateTime("01-01-2020"));
-            //repository.GetTheDetailOfSalaryForPassedGender("F");
+            ///Updating the basic pay with given name and employee id
+            model.EmpID = 4;
+            model.BasicPay = 2000000;
+            model.EmpName = "Nikhil";
+            Console.WriteLine(employeeRepository.UpdateSalary(model) ? "Updated successfully" : "Failed to update");
         }
     }
 }
